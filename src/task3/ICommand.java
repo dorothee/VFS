cacdd0438d0b5cb82c81c047076c76d2f9324c1b
dorothee <simona.poilinca@gmail.com>
2015-01-10 15:40:32 +0100

    added task 3 package with ICommand interface

---
+++ src/task3/ICommand.java
@@ -0,0 +1,17 @@
+package task3;
+
+/**
+ * Command interface with execute() method;
+ * The interfaces methods are _always_ abstract;
+ * Each class that implements ICommand has to implement (@Overrride) the methods with specific implementation.
+ * 
+ * @author dorothee
+ *
+ */
+public interface ICommand {
+
+	/**
+	 * Executes the given command.
+	 */
+	void execute();
+}
